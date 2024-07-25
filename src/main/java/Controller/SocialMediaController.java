2122869b7f0c569d8dd3f6251b3dd01b29a5157f
Andrew Donovan <a.o.donovan.music@gmail.com>
2024-07-25 01:08:23 +0000

    Create endpoints

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -17,6 +17,14 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.post("register", this::register);
+        app.post("login", this::login);
+        app.post("messages", this::createMessage);
+        app.get("messages", this::getAllMessages);
+        app.get("messages/{message_id}", this::getMessage);
+        app.delete("messages/{message_id}", this::deleteMessage);
+        app.patch("messages/{message_id}", this::updateMessage);
+        app.get("accouts/{account_id}/messages", this::getMessagesByUser);
 
         return app;
     }
@@ -29,5 +37,35 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    private void register(Context context) {
+        context.json("register");
+    }
 
+    private void login(Context context) {
+        context.json("login");
+    }
+
+    private void createMessage(Context context) {
+        context.json("create message");
+    }
+
+    private void getAllMessages(Context context) {
+        context.json("get all msgs");
+    }
+
+    private void getMessage(Context context) {
+        context.json("get a message");
+    }
+
+    private void deleteMessage(Context context) {
+        context.json("delete a message");
+    }
+
+    private void updateMessage(Context context) {
+        context.json("update a message");
+    }
+
+    private void getMessagesByUser(Context context) {
+        context.json("get a users messages");
+    }
 }
