af46952cc05ddc30d1f1caa207a7d59d32c08dd0
Andrew Donovan <a.o.donovan.music@gmail.com>
2024-07-25 01:46:59 +0000

    Write SocialMediaDAO Interface

--- src/main/java/DAO/SocialMediaDAO.java
+++ src/main/java/DAO/SocialMediaDAO.java
@@ -1,7 +1,19 @@
 package DAO;
 
+import java.util.List;
+
+import Model.Account;
+import Model.Message;
+
 interface SocialMediaDAOInter {
-    
+    public Account createAccount(String username, String password);
+    public Account login(String username, String password);
+    public Message createMessage(String messageBody, int posted_by);
+    public List<Message> getAllMessages();
+    public Message getMessageByID(int message_id);
+    public Message deleteMessage(int message_id);
+    public Message updateMessage(int message_id);
+    public List<Message> getMessagesByUser(int posted_by);
 }
 
 
